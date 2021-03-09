a93b7de4ad632d34c164570b8a3bef68fa56e423
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-09 14:59:01 +0900

    wip

--- contracts/HastenDAOToken.sol
+++ contracts/HastenDAOToken.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.7.4;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract HastenDAOToken is ERC20 {
-    // reserved by proxy
-    address internal _impl = address(0);
-
     uint256 constant INITIAL_SUPPLY = 1048576 * (10 ** 18);
 
     constructor() ERC20("Hasten DAO Token", "CODE") {
