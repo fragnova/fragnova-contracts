0621000d219f19177ea5a68d9b34735e11406a66
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-25 13:17:47 +0900

    gdeployed HastenScript

--- contracts/HastenScriptProxy.sol
+++ contracts/HastenScriptProxy.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/proxy/transparent/TransparentUpgradeable
 contract HastenScriptProxy is TransparentUpgradeableProxy {
     constructor()
         TransparentUpgradeableProxy(
-            address(0xC0DE77CE9A83B7ec9520FbA3Ca349b2cEE3a5CcC), // logic
+            address(0xC0DE00d21d6fd31F4A4d4804bc678428DA9ab545), // logic
             address(0xC0FFee00b68f0F079d904844b49a064E3046AE91), // admin
             new bytes(0)
         )
