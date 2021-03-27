42954f6288b94be44098a42bbf5e1d1b30739732
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-27 13:42:41 +0900

    wip new solidity

--- contracts/HastenScriptProxy.sol
+++ contracts/HastenScriptProxy.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/proxy/transparent/TransparentUpgradeable
 contract HastenScriptProxy is TransparentUpgradeableProxy {
     constructor()
         TransparentUpgradeableProxy(
-            address(0xC0DE0006b9055df6f94E971471F46F894a0E5B1F), // logic
+            address(0xC0DE00Eb3F0F006D54c878bD27063025f6D48b2c), // logic
             address(0xC0ffee4B437CcF6C7cE62E517D94e48c0389d1Eb), // admin
             new bytes(0)
         )
