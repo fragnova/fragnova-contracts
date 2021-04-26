f9a11657c085c0e238d58aaa81350ef777ba0163
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-04-26 12:46:26 +0900

    final deploy on mainnet

--- contracts/HastenModProxy.sol
+++ contracts/HastenModProxy.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/proxy/transparent/TransparentUpgradeable
 contract HastenModProxy is TransparentUpgradeableProxy {
     constructor()
         TransparentUpgradeableProxy(
-            address(0xc2ea7eb415Feb6E28544b729c9679bdB2d851024), // logic
+            address(0xc2ea0070878bF71c060554996DdE450f0718d612), // logic
             address(0xC0FFEEaAd4F914eD5eC6c87DfCE1e453fC16646A), // admin
             new bytes(0)
         )
