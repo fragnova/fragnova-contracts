7e7aed6391e7d48738eb8dae4755a4fc7b244ce4
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-05-12 18:12:03 +0900

    Make mod use proxy to deploy many NFT projects in 1 go

--- contracts/HastenMod.sol
+++ contracts/HastenMod.sol
@@ -3,43 +3,66 @@ pragma solidity ^0.8.0;
 import "openzeppelin-solidity/contracts/proxy/utils/Initializable.sol";
 import "openzeppelin-solidity/contracts/utils/Counters.sol";
 import "openzeppelin-solidity/contracts/utils/cryptography/ECDSA.sol";
-import "./HastenNFT.sol";
+import "./Ownable.sol";
 import "./HastenScript.sol";
 import "./Utility.sol";
 
-contract HastenMod is HastenNFT, Initializable {
-    uint8 private constant mutableVersion = 0x1;
+contract HastenMod is ERC721, Ownable, Initializable {
+    uint8 private constant dataVersion = 0x1;
 
     using SafeERC20 for IERC20;
-
-    mapping(uint256 => address) private _signers;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     // mapping for scripts storage
     mapping(uint256 => bytes) private _modData;
+    mapping(uint256 => bytes32) private _metadataURIs;
     mapping(uint256 => uint160) private _modRefs;
 
     HastenScript internal _scriptsLibrary;
+    address internal _delegate;
+    uint256 internal _publicMintingPrice;
+    uint160 internal _scriptId;
+    uint32 internal _maxPublicAmount;
+    uint32 internal _publicCap;
+    bool internal _publicMinting;
 
-    constructor(address libraryAddress, address daoAddress)
-        ERC721("Hasten Mod v0 NFT ", "MOD")
-        Ownable(address(0x7F7eF2F9D8B0106cE76F66940EF7fc0a3b23C974))
-    {
+    // constructor() ERC721("Mod", "MOD") Ownable(address(0)) {
+    //     bootstrap("Mod", "MOD", 0, address(0));
+    //     _scriptsLibrary = HastenScript(address(0));
+    // }
+
+    constructor(
+        address libraryAddress,
+        uint160 scriptId,
+        address owner
+    ) ERC721("Mod", "MOD") Ownable(owner) {
+        // this is just for testing - deployment has no constructor args (literally comment out)
+        bootstrap("Mod", "MOD", scriptId, owner);
         _scriptsLibrary = HastenScript(libraryAddress);
-        _daoToken = IERC20(daoAddress);
     }
 
-    function bootstrap() public payable initializer {
+    function bootstrap(
+        string memory name,
+        string memory symbol,
+        uint160 scriptId,
+        address owner
+    ) public payable initializer {
         // Ownable
-        Ownable._bootstrap(address(0x7F7eF2F9D8B0106cE76F66940EF7fc0a3b23C974));
-        // ERC721
-        _name = "Hasten Mod v0 NFT";
-        _symbol = "MOD";
+        Ownable._bootstrap(owner);
 
-        _scriptsLibrary = HastenScript(0xC0DE00aa1328aF9263BA5bB5e3d17521AF58b32F); // proxy script library
-        _daoToken = IERC20(address(0));
+        // Master script to mod
+        _scriptId = scriptId;
+
+        // ERC721 - this we must make sure happens only and ever in the beginning
+        // Of course being proxied it might be overwritten but if ownership is finally burned it's going to be fine!
+        _name = name;
+        _symbol = symbol;
+
+        _scriptsLibrary = HastenScript(
+            0xC0DE00aa1328aF9263BA5bB5e3d17521AF58b32F
+        ); // proxy script library
     }
 
     function tokenURI(uint256 tokenId)
@@ -54,79 +77,56 @@ contract HastenMod is HastenNFT, Initializable {
             "HastenScript: URI query for nonexistent token"
         );
 
-        bytes storage data = _modData[_modRefs[tokenId]];
-        bytes memory ipfsCid = new bytes(32);
-        for (uint256 i = 0; i < 32; i++) {
-            ipfsCid[i] = data[i + 1]; // skip 1 byte, version number
-        }
         return
             string(
                 abi.encodePacked(
                     "ipfs://",
                     Utility.toBase58(
-                        abi.encodePacked(uint8(0x12), uint8(0x20), ipfsCid)
+                        abi.encodePacked(
+                            uint8(0x12),
+                            uint8(0x20),
+                            _metadataURIs[tokenId]
+                        )
                     )
                 )
             );
     }
 
-    function _scriptId(uint256 modId) private view returns (uint160) {
-        uint160 res;
-        bytes storage mutableData = _modData[_modRefs[modId]];
-        {
-            bytes memory scriptIdBytes = new bytes(20);
-            for (uint256 i = 0; i < 20; i++) {
-                scriptIdBytes[i] = mutableData[i + 33]; // skip 33 bytes, version number and ipfs cid
-            }
-            assembly {
-                res := mload(add(scriptIdBytes, 20))
-            }
-        }
-        return res;
-    }
-
     function dataOf(uint256 modId)
         public
         view
-        returns (bytes memory immutableData, bytes memory mutableData)
+        returns (
+            bytes memory immutableData,
+            bytes memory mutableData,
+            bytes memory modData
+        )
     {
         require(
             _exists(modId),
             "HastenScript: script query for nonexistent token"
         );
 
-        mutableData = _modData[_modRefs[modId]];
-        (immutableData, ) = _scriptsLibrary.dataOf(_scriptId(modId));
+        modData = _modData[_modRefs[modId]];
+        (immutableData, mutableData) = _scriptsLibrary.dataOf(_scriptId);
     }
 
-    function setDelegate(uint256 scriptId, address delegate) public {
-        require(
-            msg.sender == _scriptsLibrary.ownerOf(scriptId),
-            "HastenMod: Only the owner of the script can set signer delegate"
-        );
-
-        _signers[scriptId] = delegate;
+    function setDelegate(address delegate) public onlyOwner {
+        _delegate = delegate;
     }
 
     function _upload(
-        uint160 scriptId,
         bytes32 ipfsMetadata,
         bytes memory environment,
-        uint256 amount
+        uint32 amount
     ) internal {
         uint160 dataHash =
             uint160(
-                uint256(keccak256(abi.encodePacked(scriptId, environment)))
+                uint256(keccak256(abi.encodePacked(_scriptId, environment)))
             );
 
         // store only if not already present
         if (_modData[dataHash].length == 0) {
-            _modData[dataHash] = abi.encodePacked(
-                mutableVersion,
-                ipfsMetadata,
-                scriptId,
-                environment
-            );
+            _modData[dataHash] = abi.encodePacked(dataVersion, environment);
         }
 
         for (uint256 i = 0; i < amount; i++) {
@@ -136,21 +136,16 @@ contract HastenMod is HastenNFT, Initializable {
             _mint(msg.sender, newItemId);
 
             _modRefs[newItemId] = dataHash;
+            _metadataURIs[newItemId] = ipfsMetadata;
         }
     }
 
     function upload(
-        uint160 scriptId,
         bytes32 ipfsMetadata,
         bytes memory environment,
-        uint256 amount
-    ) public {
-        require(
-            msg.sender == _scriptsLibrary.ownerOf(scriptId),
-            "HastenMod: Only the owner of the script can upload mods"
-        );
-
-        _upload(scriptId, ipfsMetadata, environment, amount);
+        uint32 amount
+    ) public onlyOwner {
+        _upload(ipfsMetadata, environment, amount);
     }
 
     /*
@@ -158,10 +153,9 @@ contract HastenMod is HastenNFT, Initializable {
     */
     function uploadWithDelegateAuth(
         bytes memory signature,
-        uint160 scriptId,
         bytes32 ipfsMetadata,
         bytes memory environment,
-        uint256 amount
+        uint32 amount
     ) public {
         bytes32 hash =
             ECDSA.toEthSignedMessageHash(
@@ -169,7 +163,7 @@ contract HastenMod is HastenNFT, Initializable {
                     abi.encodePacked(
                         msg.sender,
                         Utility.getChainId(),
-                        scriptId,
+                        _scriptId,
                         ipfsMetadata,
                         environment,
                         amount
@@ -177,12 +171,55 @@ contract HastenMod is HastenNFT, Initializable {
                 )
             );
         require(
-            _signers[scriptId] != address(0x0) &&
-                _signers[scriptId] == ECDSA.recover(hash, signature),
+            _delegate != address(0x0) &&
+                _delegate == ECDSA.recover(hash, signature),
             "HastenMod: Invalid signature"
         );
 
-        _upload(scriptId, ipfsMetadata, environment, amount);
+        _upload(ipfsMetadata, environment, amount);
+    }
+
+    /*
+        This is to allow public sales.
+    */
+    function uploadForEth(
+        bytes32 ipfsMetadata,
+        bytes memory environment,
+        uint32 amount
+    ) public payable {
+        require(_publicMinting, "Public minting not allowed");
+        require(
+            _tokenIds.current() < _publicCap,
+            "Public minting limit has been reached"
+        );
+        require(amount <= _maxPublicAmount, "Invalid amount");
+        uint256 price = amount * _publicMintingPrice;
+        require(msg.value >= price, "Not enough value");
+        _upload(ipfsMetadata, environment, amount);
+    }
+
+    function setPublicSale(
+        bool enabled,
+        uint256 price,
+        uint32 maxAmount,
+        uint32 cap
+    ) public onlyOwner {
+        _publicMinting = enabled;
+        _publicMintingPrice = price;
+        _maxPublicAmount = maxAmount;
+        _publicCap = cap;
+    }
+
+    /*
+        Allow owners to update metadata
+    */
+    function updateMetadata(uint256 tokenId, bytes32 metadata) public {
+        require(_exists(tokenId), "HastenScript: nonexistent token");
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "HastenScript: not token owner"
+        );
+        _metadataURIs[tokenId] = metadata;
     }
 
     // reward the owner of the Script
@@ -191,34 +228,5 @@ contract HastenMod is HastenNFT, Initializable {
         address from,
         address to,
         uint256 tokenId
-    ) internal override {
-        // ensure not a mint or burn
-        if (
-            to != address(0) &&
-            from != address(0) &&
-            address(_daoToken) != address(0)
-        ) {
-            address scriptOwner = _scriptsLibrary.ownerOf(_scriptId(tokenId));
-            if (
-                _rewardBlocks[scriptOwner] != block.number &&
-                _daoToken.balanceOf(address(this)) > _reward
-            ) {
-                _daoToken.safeIncreaseAllowance(address(this), _reward);
-                _daoToken.safeTransferFrom(address(this), scriptOwner, _reward);
-                _rewardBlocks[scriptOwner] = block.number;
-            }
-        } else if (to == address(0)) {
-            // burn, cleanup some memory
-            // altho big storage is not cleared
-            _modRefs[tokenId] = 0x0;
-        }
-    }
-
-    function setScriptOwnerReward(uint256 amount) public onlyOwner {
-        _reward = amount;
-    }
-
-    function getScriptOwnerReward() public view returns (uint256) {
-        return _reward;
-    }
+    ) internal override {}
 }
