37ec8e8e4d26a0e733a7e6f32671173e0bca8be0
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-07-23 09:46:36 +0900

    make FragmentNFT an enumerable

--- contracts/FragmentNFT.sol
+++ contracts/FragmentNFT.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.0;
 
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Ownable.sol";
 
-abstract contract FragmentNFT is ERC721, Ownable {
+abstract contract FragmentNFT is ERC721Enumerable, Ownable {
     using SafeERC20 for IERC20;
 
     function recoverERC20(address tokenAddress, uint256 tokenAmount)
