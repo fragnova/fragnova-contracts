a1920172da4cb7e050cbd846786fea197a4146b6
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-10-10 21:20:44 +0900

    print tx info of entity minting

--- test/program.js
+++ test/program.js
@@ -369,6 +369,7 @@ contract("Fragment", accounts => {
     const tx = await entity.methods
       .mint(signature, empty, 1)
       .send({ from: accounts[4], gas: 800000, value: web3.utils.toWei("0.1", "ether") });
+    console.log(tx);
     const res = await entity.methods.tokenURI(2).call();
     console.log(res);
     assert.equal(res, 'https://metadata.fragments.foundation/?ch=0x01&id=0x02&e=' + fragmentOneEntity.toLowerCase() + '&m=0xfa321bd82f92ef059c267763b69b7c27d6c70bd1ea86b94194ff74884fdd1ae0&d=0x23');
