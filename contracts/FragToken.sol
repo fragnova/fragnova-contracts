12c0221293ca90e6484e44d78f022ceb1bc0cbb6
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2021-12-31 22:56:07 +0900

    fragtoken wip

--- contracts/FragToken.sol
+++ contracts/FragToken.sol
@@ -4,11 +4,40 @@
 pragma solidity ^0.8.7;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract FragmentDAOToken is ERC20 {
-    uint256 constant INITIAL_SUPPLY = 100000000 * (10 ** 18);
+contract FragmentDAOToken is ERC20, ERC20Permit, ERC20Votes {
+    uint256 constant INITIAL_SUPPLY = 1000000000 * (10**18);
 
-    constructor() ERC20("Fragments Foundation Token", "FRAG") {
+    constructor()
+        ERC20("Fragments Foundation Token", "FRAG")
+        ERC20Permit("Fragments Foundation Token")
+    {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    // The functions below are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
